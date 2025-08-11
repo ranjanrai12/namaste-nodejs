89a89a9ded929755fa49d5df4885360f96c9a3b1
ranjanrai <ranjankumarrai1992@gmail.com>
2025-08-11 22:58:30 +0530

    find, findOne,findById Task completed

--- devtinder-app/app.js
+++ devtinder-app/app.js
@@ -7,13 +7,61 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.post("/signup", async (req, res) => {
-
-    const user = new User(req.body);
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User created successfully");
   } catch (error) {
     res.status(400).send("Error creating user: " + error.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  //   try {
+  // find will return an array of documents
+  //     const user = await User.find({ email: req.body.email });
+  //     if (user.length === 0) {
+  //       res.status(404).send("User not found");
+  //     } else {
+  //       res.send(user);
+  //     }
+  //   } catch (err) {
+  //     res.status(500).send("Error fetching users: " + err.message);
+  //   }
+  try {
+    // findOne will return a single document or null
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Error fetching user: " + err.message);
+  }
+});
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.send("Error fetching user by ID: " + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    if (allUsers.length === 0) {
+      res.status(404).send("No users found");
+    } else {
+      res.send(allUsers);
+    }
+  } catch (error) {
+    res.status(500).send("Error fetching feed: " + error.message);
   }
 });
 
